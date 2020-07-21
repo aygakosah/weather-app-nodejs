52f4e977f61716bc1d28ac11bee4ebf470fe1f9b
unknown <gideonakosah37@gmail.com>
2020-07-21 15:55:46 +0400

    Removed unnecessary fetch function

--- amk/js/app.js
+++ amk/js/app.js
@@ -1,12 +1,3 @@
-// fetch("http://localhost:3000/weather?address=!").then((response)=>{
-//     response.json().then((data)=>{
-//         if(data.error){
-//             return console.log(data.error)
-//         }
-//         console.log(data)
-//     })
-// })
-
 
 const weatherForm =document.querySelector("form")
 const search = document.querySelector("input")
